38c54f7c99e1800fabd58ee2b73a1cbd3b4e6dc8
meills <amelia.ohml@gmail.com>
2020-06-16 22:56:53 +0800

    fixed security error

--- .history/src/components/Credits_20200616225628.js
+++ .history/src/components/Credits_20200616225628.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Credits() {
     return (<div className='credits'>
-                <b>CREATED BY <a href='https://github.com/meills' target='_blank' rel='noopener noreferrer'>MEILLS</a></b>
+                <b>CREATED BY <a href='https://github.com/meills' target='_blank' rel='noopener noreferrer;>MEILLS</a></b>
             </div>)
 }
 
